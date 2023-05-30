5c432e593ac8ad45bef1009167350aaebc9a0a0f
Mark Tranter <mark.a.tranter@gmail.com>
2023-05-31 08:45:06 +1000

    Fix tf vars

--- infra/modules/kafka/confluent/schema-registry/main.tf
+++ infra/modules/kafka/confluent/schema-registry/main.tf
@@ -37,7 +37,7 @@ resource "confluent_role_binding" "schema_manager_environment_admin" {
 }
 
 resource "confluent_api_key" "schema_manager_api_key" {
-  display_name = "${var.project-name}-${var.environment}-schema-manager-schema-registry-api-key"
+  display_name = "${var.project_name}-${var.environment_name}-schema-manager-schema-registry-api-key"
   description  = "Schema Registry API Key that is owned by 'schema-manager' service account"
   owner {
     id          = confluent_service_account.schema_manager.id
