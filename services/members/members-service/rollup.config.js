0142002986169c018e9ae020a5f74bfe82bfbff2
Mark Tranter <mark.a.tranter@gmail.com>
2023-06-04 13:25:59 +1000

    try different rollup config

--- services/members/members-service/rollup.config.js
+++ services/members/members-service/rollup.config.js
@@ -4,16 +4,23 @@ const nodeResolve = require("@rollup/plugin-node-resolve");
 const commonJs = require("@rollup/plugin-commonjs");
 const json = require("@rollup/plugin-json");
 
-const entryPoints = ["src/api/api-handler.ts", "src/tx-outbox/tx-outbox-handler.ts"];
+const entryPoints = [
+  "src/api/api-handler.ts",
+  "src/tx-outbox/tx-outbox-handler.ts",
+];
 const configs = entryPoints.map((entryPoint) => ({
   input: entryPoint,
   output: {
     dir: "dist",
     format: "cjs",
-    sourcemap: true,
-    globals: {crypto: 'crypto'},
+    sourcemap: true
   },
-  external: ['crypto'],
-  plugins: [typescript(), commonJs(), nodeResolve({ preferBuiltins: true }), json()],
+  external: ["crypto"],
+  plugins: [
+    typescript(),
+    commonJs(),
+    nodeResolve({ preferBuiltins: true }),
+    json(),
+  ],
 }));
 module.exports = configs;
