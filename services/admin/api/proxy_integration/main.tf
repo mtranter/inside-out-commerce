41b616fb0a6307d6c267ab2064f9653850fe8c4d
Mark Tranter <mark.a.tranter@gmail.com>
2023-07-04 16:21:09 +1000

    Add integratoin method to Proxy Integration TF module

--- services/admin/api/proxy_integration/main.tf
+++ services/admin/api/proxy_integration/main.tf
@@ -29,16 +29,17 @@ resource "aws_api_gateway_method" "parent" {
 }
 
 resource "aws_api_gateway_integration" "parent" {
-  rest_api_id = var.rest_api_id
-  resource_id = aws_api_gateway_resource.parent.id
-  http_method = aws_api_gateway_method.parent.http_method
-  type        = "HTTP_PROXY"
-  uri         = var.uri
+  rest_api_id             = var.rest_api_id
+  resource_id             = aws_api_gateway_resource.parent.id
+  http_method             = aws_api_gateway_method.parent.http_method
+  integration_http_method = "ANY"
+  type                    = "HTTP_PROXY"
+  uri                     = var.uri
 }
 
 resource "aws_api_gateway_resource" "proxy" {
   rest_api_id = var.rest_api_id
-  parent_id = aws_api_gateway_resource.parent.id
+  parent_id   = aws_api_gateway_resource.parent.id
   path_part   = "{proxy+}"
 }
 
@@ -50,11 +51,12 @@ resource "aws_api_gateway_method" "proxy" {
 }
 
 resource "aws_api_gateway_integration" "proxy" {
-  rest_api_id = var.rest_api_id
-  resource_id = aws_api_gateway_resource.proxy.id
-  http_method = aws_api_gateway_method.proxy.http_method
-  type        = "HTTP_PROXY"
-  uri         = var.uri
+  rest_api_id             = var.rest_api_id
+  resource_id             = aws_api_gateway_resource.proxy.id
+  http_method             = aws_api_gateway_method.proxy.http_method
+  type                    = "HTTP_PROXY"
+  integration_http_method = "ANY"
+  uri                     = var.uri
 }
 
 // outputs
