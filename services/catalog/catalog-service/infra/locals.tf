97c4f27796767524f48fca24eb37a4095df7cdd7
Mark Tranter <mark.a.tranter@gmail.com>
2023-07-06 13:08:56 +1000

    Change topics and subdomain name

--- services/catalog/catalog-service/infra/locals.tf
+++ services/catalog/catalog-service/infra/locals.tf
@@ -1,7 +1,7 @@
 locals {
-  topic_prefix  = "com.insideoutcommerce.products."
+  topic_prefix  = "com.insideoutcommerce.catalog."
   topic_name    = "${local.topic_prefix}ProductData"
-  api_subdomain = "products"
+  api_subdomain = "catalog"
   execute_scope = "api.execute"
   stage_name    = "live"
 }
