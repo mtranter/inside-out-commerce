dfeec4470afad2b9169c0c066a36fd342bdccd23
Mark Tranter <mark.a.tranter@gmail.com>
2023-07-07 15:13:09 +1000

    Fix build

--- services/admin/ui/src/App.tsx
+++ services/admin/ui/src/App.tsx
@@ -1,6 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { STSClient, AssumeRoleWithWebIdentityCommand, Credentials } from "@aws-sdk/client-sts";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function App() {
   const { loginWithRedirect, isAuthenticated, getIdTokenClaims } = useAuth0();
