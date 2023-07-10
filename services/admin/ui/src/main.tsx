54647aebe49f8280f57470ceb29d0b7048fc32d1
Mark Tranter <mark.a.tranter@gmail.com>
2023-07-10 10:57:15 +1000

    Update auth0 cache location

--- services/admin/ui/src/main.tsx
+++ services/admin/ui/src/main.tsx
@@ -12,6 +12,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
       clientId="378ri5r4qmg0d44c9d5k58rb7h"
       issuer="https://cognito-idp.ap-southeast-2.amazonaws.com/ap-southeast-2_V1m1AEYXE"
       useRefreshTokens={true}
+      cacheLocation={"localstorage"}
       authorizationParams={{
         audience: "378ri5r4qmg0d44c9d5k58rb7h",
         redirect_uri: window.location.origin,
